28ef263f3df2e5cccb09f39608575ee511862245
mrsfable <56641377+mrsfable@users.noreply.github.com>
2019-10-29 08:34:12 +0100

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-RECEIVER = "christian@fableplus.com"
-
-SMTP_USER = "no-reply@fableplus.com"
-SMTP_PASSWORD = "gGNDUY5Vjsxqyaog"
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 587
-SMTP_STARTTLS = os.environ.get("SMTP_STARTTLS", "true").lower() == "true"
